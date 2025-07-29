2d94f53a7b5e5f988d4e5a905ca97faf511dc878
Vergesst <3224171967@qq.com>
2025-07-29 08:00:46 +0800

    convert groovy-build.gradle & settings.gradle -> build.gradle.kts & settings.gradle.kts

---
+++ src/client/java/vergisst/minecraftmod/weaponthrow/client/WeaponThrowClient.java
@@ -0,0 +1,10 @@
+package vergisst.minecraftmod.weaponthrow.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WeaponThrowClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/vergisst/minecraftmod/weaponthrow/WeaponThrow.java
@@ -0,0 +1,10 @@
+package vergisst.minecraftmod.weaponthrow;
+
+import net.fabricmc.api.ModInitializer;
+
+public class WeaponThrow implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
