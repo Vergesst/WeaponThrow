89022dbb29486cb157631c61b328e914b5469ee2
Vergesst <3224171967@qq.com>
2025-10-22 17:46:31 +0800

    refactor: rewrite most parts with kotlin

---
+++ src/main/kotlin/vergisst/minecraftmod/weaponthrow/capabilities/PlayerThrowData.kt
@@ -0,0 +1,46 @@
+package vergisst.minecraftmod.weaponthrow.capabilities
+
+import net.minecraft.entity.player.PlayerEntity
+import net.minecraft.item.ItemStack
+import net.minecraft.util.math.MathHelper
+import vergisst.minecraftmod.weaponthrow.handler.ConfigRegistry
+import vergisst.minecraftmod.weaponthrow.packets.State
+
+class PlayerThrowData(val player: PlayerEntity) {
+    // client
+    var maxCharge = -1
+
+    // both
+    var action = State.NONE
+
+    // Server
+    var chargeTime = -1
+
+    var item: ItemStack = ItemStack.EMPTY
+
+    fun startCharging(stack: ItemStack) {
+        item = stack.copy()
+        chargeTime = getMaximumCharge(player)
+    }
+
+    fun resetCharging() {
+        action = if(action == State.DURING) State.FINISH else State.NONE
+        item = ItemStack.EMPTY
+        chargeTime = -1
+    }
+
+    fun getChargingStack(): ItemStack = item
+
+    companion object {
+        fun getMaximumCharge(player: PlayerEntity): Int =
+            MathHelper.floor(
+                player
+                    .attackCooldownProgressPerTick * ConfigRegistry
+                    .COMMON
+                    .get()
+                    .times
+                    .castTimeMultiplier
+            )
+
+    }
+}
